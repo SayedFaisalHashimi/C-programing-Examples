01ccf637084345ef5872cd1f1d3ba12067836bdc
Sayed Faisal Hashimi <140006205+SayedFaisalHashimi@users.noreply.github.com>
2025-10-25 18:17:36 +0300

    File Managings

---
+++ C_Functions_Examples/Function Recursion/Ahmad.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * File: simple_sum.c
+ * Description:
+ *   This program reads two integers from the user and prints their sum.
+ */
+
+int main() {
+    int a, b, sum;
+
+}
